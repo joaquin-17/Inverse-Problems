f683014665b06f9a6994252ba7a6d25e9a3c3354
Joaquín Acedo <aacedo@ualberta.ca>
2023-03-12 10:10:16 -0600

    Continue Trying

--- A3.jl
+++ A3.jl
@@ -9,4 +9,11 @@ Assignment3: Norms
 =#
 
 
-using LinearAlgebra, 
+using LinearAlgebra, DataFrames, Plots,DelimitedFiles
+
+data=readdlm("data.txt");
+df=DataFrame("x"=>data[:,1], "y"=>data[:,2]);
+
+scatter(df[!,"x"], df[!,"y"],color=:green, seriestype=:scatter)
+xlabel!("x")
+ylabel!("y")
