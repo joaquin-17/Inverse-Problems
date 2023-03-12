45391a1a62c6d7383832523e203532fd82758081
Joaquín Acedo <aacedo@ualberta.ca>
2023-03-12 10:43:30 -0600

    Folder for assigment 3 of GIP, which is related

---
+++ Norms/A3.jl
@@ -0,0 +1,19 @@
+#=
+
+GEOPH 531 : Geophysical inverse problems
+Department of Physics
+2023
+
+Assignment3: Different Norms
+
+=#
+
+
+using LinearAlgebra, DataFrames, Plots,DelimitedFiles
+
+data=readdlm("data.txt");
+df=DataFrame("x"=>data[:,1], "y"=>data[:,2]);
+
+scatter(df[!,"x"], df[!,"y"],color=:green, seriestype=:scatter)
+xlabel!("x")
+ylabel!("y")
