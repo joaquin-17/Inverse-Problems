43973a7bb732af479a0f381d3055acbe1ac79ae3
Joaquin Acedo <aacedo@ualberta.ca>
2023-04-10 14:33:07 -0600

    ADMM using CG explicit, working!

--- Fourier Reconstruction/ADMM.jl
+++ Fourier Reconstruction/ADMM.jl
@@ -56,9 +56,11 @@ m0=zeros(length(d_obs));
 #m0=F*(d_obs);
 #d_rec=F'*(m);
 
+SoftThresholding(x,ρ,λ) = sign(x)*max(abs(x)- (ρ/(2*λ)),0)
 
+#=
+# Alternative Direction Method of Multipliers: Inveting Matrices
 
-# Alternative Direction Method of Multipliers:
 
 SoftThresholding(x,ρ,λ) = sign(x)*max(abs(x)- (ρ/(2*λ)),0)
 
@@ -84,6 +86,11 @@ function ADMM(A,y,m0; ρ= 1.0, λ=0.5,tol=1e-8, Ni=150)
     return z;
 
 end
+
+=#
+
+
+# Alternative Direction Method of Multipliers: With explicit matrices CG
 
 
 function cgaq(A,y,it)
@@ -115,7 +122,6 @@ function cgaq(A,y,it)
 end
 
 
-
 function ADMM_CG(A,y,m0; ρ= 1.0, λ=0.5,tol=1e-8, Ni=150, Ne=50)
 
     
@@ -124,19 +130,12 @@ function ADMM_CG(A,y,m0; ρ= 1.0, λ=0.5,tol=1e-8, Ni=150, Ne=50)
     I = diagm( ones(size(A,2)));
     Ac= vcat(A, sqrt(ρ)*I);
     yc=vcat(y, sqrt(ρ)* (z .- u))
-    #G=A'*A;
-    #I = diagm( ones(size(G,1)));
 
-    
     for k=1:Ni;
-
-
-        Ac= vcat(A, sqrt(ρ)*I);
         yc=vcat(y, sqrt(ρ)* (z-u));
         x= cgaq(Ac,yc,Ne) #nv(G +ρ*I)*(A'*(y) .+ ρ*(z - u)); #x-update
         z= SoftThresholding.(x .+ u ,ρ,λ)  # z-update
         u= u .+ (x-z); #dual update, lagrange multiploier
-
     end
 
     return z;
@@ -146,5 +145,168 @@ end
 
 
 
-m1= ADMM(A,y,m0, ρ= 1.0 , λ=1.8,Ni=50) # This worlks
-m2= ADMM_CG(A,y,m0, ρ= 1.0 , λ= 1.8, Ni=50, Ne=50) # This worlks
+# # Alternative Direction Method of Multipliers: With Operators
+
+
+function Sampling(in::Vector)
+
+    cutoff = 1e-10
+    i = 1
+
+    wd = zeros(Real,length(in))
+    for i = 1 : length(in)
+        a =(in[i])^2;
+        if (abs(a) > cutoff)
+            wd[i] = 1.0;
+        end
+    end
+    return wd;
+end
+
+S=Sampling(d_obs);
+
+function SamplingOp(in:: Vector, adj; t::Vector, Ni=512)
+
+
+    No=length(t);
+    T=zeros(Int,(No,Ni));
+
+    for i=1:size(T,1);
+        T[i,Int(t[i])+1] = 1
+    end
+       
+    if (adj)
+
+        return T'*in;
+    else
+        return T*in;
+
+    end
+end
+
+function FFTOp(in,adj;normalize=true)
+	norm = normalize ? sqrt(length(in[:])) : 1.
+	if (adj)
+		out = fft(in)/norm
+	else
+		out = bfft(in)/norm
+	end
+	return out
+end	
+
+
+
+function WeightingOp(in,adj;w=1.0)
+
+        return in.*w;
+end
+
+
+
+function InnerProduct(in1,in2)
+    
+    return convert(Float32,real(sum(conj(in1[:]).*in2[:])))
+
+end
+
+
+function LinearOperator(in,operators,parameters;adj=true)
+	if adj
+		d = copy(in)
+		m = [];
+		for j = 1 : 1 : length(operators)
+			op = operators[j]
+			m = op(d,true;parameters[j]...)
+			d = copy(m)
+		end
+		return m
+	else
+		m = copy(in)
+		d = [];
+		for j = length(operators) : -1 : 1
+			op = operators[j]
+			d = op(m,false;parameters[j]...)
+			m = copy(d)
+		end
+		return d
+	end
+
+end
+
+
+
+function CGLS(d_obs, operators,parameters; μ=0.5, Ni=100, tol=1.0e-15)
+
+    m=zeros(length(d_obs));
+    r= d_obs - LinearOperator(m,operators,parameters,adj=false);
+    s =  LinearOperator(r,operators,parameters,adj=true) -μ*m;
+    p=copy(s);
+
+    gamma= InnerProduct(s,s);
+    norms0=sqrt(gamma); #norm of the gradient is used to stop.
+    k=0;
+    flag=0;
+    J=zeros(Ni);
+
+
+    while k < Ni && flag == 0
+        
+        q = LinearOperator(p,operators,parameters,adj=false);
+        delta= InnerProduct(q,q) + μ*InnerProduct(p,p);
+
+        if delta == 0
+            delta=1.e-10;
+        end
+
+        alpha= gamma/delta;
+        m = m + alpha*p;
+        r = r - alpha*q;
+        s =  LinearOperator(r,operators,parameters,adj=true) -μ*m;
+        gamma1  = InnerProduct(s,s);
+        norms  = sqrt(gamma1);
+        beta = gamma1/gamma;
+        gamma = gamma1;
+        p = s + beta*p;
+        #if norms <= norms0*tol
+         #   println("Loop ended causde tolerance was reached",k)
+          #  break;
+        #end
+        k = k+1;
+        println(k)
+        error = LinearOperator(m,operators,parameters,adj=false) - d_obs ;
+        J[k] = sum( abs.(error[:]).^2 ) + μ*sum( (abs.(m)).^2);
+
+    end
+
+    return m, J
+end
+
+
+function ADMM_CGLS(d_obs,operators,parameters; ρ= 1.0, λ= 1.8,tol=1e-8, Ni=50,Ne=50)
+    
+    
+    u=zeros(length(m0)); 
+    z=copy(m0);
+    
+    for k=1:Ni;
+
+        d_obs=vcat(d_obs, ρ*(z-u));
+        x, J= CGLS(d_obs,operators, parameters; μ=λ , Ni=Ni, tol=1.0e-15)
+        z= SoftThresholding.(x .+ u ,ρ,λ)  # z-update
+        u= u .+ (x-z); #dual update, lagrange multiploier
+    end
+
+    return z;
+
+end
+
+
+
+operators=[WeightingOp, FFTOp];
+parameters=[Dict(:w=>S), Dict(:normalize=>true)];
+
+
+#m1= ADMM(A,y,m0, ρ= 1.0 , λ=1.8,Ni=50) # This worlks
+#m2= ADMM_CG(A,y,m0, ρ= 1.0 , λ= 1.8, Ni=50, Ne=50) # This worlks
+m3= ADMM_CGLS(d_obs,operators,parameters)
+
