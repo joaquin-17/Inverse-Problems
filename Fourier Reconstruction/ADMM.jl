98e00697ae4796140b26e0dc74cf638cf8217d7a
Joaquin Acedo <aacedo@ualberta.ca>
2023-04-10 11:42:48 -0600

    Added matrix based CG

--- Fourier Reconstruction/ADMM.jl
+++ Fourier Reconstruction/ADMM.jl
@@ -43,8 +43,6 @@ end
 
 
 
-<<<<<<< HEAD
-=======
 
 y=signal;
 No=length(y);
@@ -60,17 +58,12 @@ m0=zeros(length(d_obs));
 
 
 
->>>>>>> 1446a8a468d528ab3a207edf9cc736623531599f
 # Alternative Direction Method of Multipliers:
 
-SoftThresholding(x,ρ,λ) = sign(x)*max(abs(x)- λ*ρ,0)
+SoftThresholding(x,ρ,λ) = sign(x)*max(abs(x)- (ρ/(2*λ)),0)
 
 
-<<<<<<< HEAD
-function ADMM(A,y,m0; ρ= 1.0, λ=0.5,tol=1e-8, Ni=200)
-=======
-function LASSO_ADMM(A,y,m0; ρ= 1.0, λ=0.5,tol=1e-8, Ni=200)
->>>>>>> 1446a8a468d528ab3a207edf9cc736623531599f
+function ADMM(A,y,m0; ρ= 1.0, λ=0.5,tol=1e-8, Ni=150)
 
     
     u=zeros(length(m0)); 
@@ -84,7 +77,7 @@ function LASSO_ADMM(A,y,m0; ρ= 1.0, λ=0.5,tol=1e-8, Ni=200)
 
         x= inv(G +ρ*I)*(A'*(y) .+ ρ*(z - u)); #x-update
         z= SoftThresholding.(x.+u ,ρ,λ)  # z-update
-        u= u + (x-z); #dual update
+        u= u + (x-z); #dual update, lagrange multiploier
 
     end
 
@@ -93,28 +86,53 @@ function LASSO_ADMM(A,y,m0; ρ= 1.0, λ=0.5,tol=1e-8, Ni=200)
 end
 
 
+function cgaq(A,y,it)
+    
+    error=zeros(it)
+    x=zeros(size(A,2));
+
+    #it=length(x)
+    s=y.-(A*x);              #s represents the residual error, this lives in the space of the data
+    p=A'*s;                   #By applying A', we are moving the residual back to the model space. Getting a model error(?)
+    r=p;                      #    ??
+    q=A*p                     #We are taking the model error(?) and we are projecting it back to the data space.
+    old=r'*r;                 #norm squared of the model error(?) are we looking to make this small as well?
+   #Conjugate gradient loop
+   for k in 1:it
+        alpha=(r'*r)/(q'*q);   #Ratio between the sq norm of the model error(?) and the sqnorm of its projection onto data space
+        x=x+alpha.*p;          #We update our initial model guess multiplying alpha by the original model error (?)
+        s=s-alpha.*q;          #We update the data error subtracting alpha*model error projected on data
+        r= A'*s;               #We project the updated data error into the model space.
+        new=r'*r;
+        beta=new/old;          #Ratio between the new and old norm of the model error (?)
+        old=new;               #Variable update
+        p = r.+(beta.*p);      #Updating the model error by advancing using the ratio between new and old norm.
+        q= A*p;
+        println("Iteration",k)
+        #error[k]= new                #Taking the new model error and projecting it into the data space.
+   end
+   return x
+end
 
 
 
-<<<<<<< HEAD
-function ADMM(y,m0, operators,parameters; ρ= 1.0, λ=0.5,tol=1e-8, Ni=200)
-=======
-function LASSO_ADMM(y,m0, operators,parameters; ρ= 1.0, λ=0.5,tol=1e-8, Ni=200)
->>>>>>> 1446a8a468d528ab3a207edf9cc736623531599f
+function ADMM_CG(A,y,m0; ρ= 1.0, λ=0.5,tol=1e-8, Ni=150)
 
     
     u=zeros(length(m0)); 
     z=copy(m0);
-
-    G=A'*A;
-    I = diagm( ones(size(G,1)));
+    I = diagm( ones(size(A,2)));
+    Ac= vcat(A, sqrt(ρ)*I);
+    yc=vcat(y, sqrt(ρ)* (z- u))
+    #G=A'*A;
+    #I = diagm( ones(size(G,1)));
 
     
     for k=1:Ni;
 
-        x= inv(G +ρ*I)*(A'*(y) .+ ρ*(z - u)); #x-update
+        x= cgaq(Ac,yc,200) #nv(G +ρ*I)*(A'*(y) .+ ρ*(z - u)); #x-update
         z= SoftThresholding.(x.+u ,ρ,λ)  # z-update
-        u= u + (x-z); #dual update
+        u= u + (x-z); #dual update, lagrange multiploier
 
     end
 
@@ -122,18 +140,8 @@ function LASSO_ADMM(y,m0, operators,parameters; ρ= 1.0, λ=0.5,tol=1e-8, Ni=200
 
 end
 
-<<<<<<< HEAD
-y=signal;
-No=length(y);
-Ni=512;
-F=DFT_matrix(Ni);
-T=SamplingOp(signal,Ni)
-A= T*F'
-#m, J =IRLS(A,y,Niter=15,λ=0.1);
-d_obs=T'*y;
-m0=zeros(length(d_obs));
-#m0=F*(d_obs);
-#d_rec=F'*(m);
 
-=======
->>>>>>> 1446a8a468d528ab3a207edf9cc736623531599f
+
+
+m1= ADMM(A,y,m0, ρ= 1.0 , λ=1.8,Ni=50) # This worlks
+m2= ADMM_CG(A,y,m0, ρ= 1.0 , λ=1.8, Ni=50) # This worlks
