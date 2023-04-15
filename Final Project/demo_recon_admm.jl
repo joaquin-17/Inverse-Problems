d7cb7550d7f31209113939eed69c948e69237b96
Joaquín Acedo <aacedo@ualberta.ca>
2023-04-15 16:49:34 -0600

    New tests

--- Final Project/demo_recon_admm.jl
+++ Final Project/demo_recon_admm.jl
@@ -11,15 +11,19 @@ include("LocalFFTOp3d-padding.jl")
 
 println("1) Generate synthetic data:")
 
-dt= 0.002;
+#dt= 0.002;
+#d = SeisLinearEvents(;nt=250, nx1=128,dx1=12.5, nx2=128,dx2=12.5,tau=[0.25,0.4],p1=[-0.0001,-0.00015],p2=[0.0001,0.00015]);
+#shot=d;
+#shot=d[:,64,:];
 
-d = SeisLinearEvents(;nt=250, nx1=128,dx1=12.5, nx2=128,dx2=12.5,tau=[0.25,0.4],p1=[-0.0001,-0.00015],p2=[0.0001,0.00015]);
-
-shot=d;
-
-shot=d[:,64,:];
+d=SeisParabEvents(tau=[0.256, 0.512, 0.768] ,amp=[1.0, 0.5, -1.0], p2=[0.45,0.0, -0.45], 
+p1=[0.015,0.0, -0.015], dt=dt, nt=512,  
+dx1=10.0, nx1=128, dx2=10.0, nx2=128, f0= 25.0 );
 
 dobs = copy(d);
+
+#shot=d;
+#shot=d[:,32,:];
 
 nt,nx1,nx2=size(d)
 for i1=1:nx1
@@ -31,12 +35,15 @@ for i1=1:nx1
     end
 end
 
-
+    
+    
 S = CalculateSampling(dobs);
 dobs = S.*d; 
 patch_size=(64,32,32); #Patch size in LocalFourier Operator
 Noverlap=(32,16,16); #Overlap of patches in LocalFourier Operator
 dims=size(d); #Parameter to  ensure right dimensions with the LocalFourier transform
+    
+
 
 println("3) Get parameters:")
 
--- Final Project/demo_recon_fista.jl
+++ Final Project/demo_recon_fista.jl
@@ -11,12 +11,12 @@ println("1) Generate synthetic data:")
 
 dt= 0.002;
 
-#d=SeisParabEvents(tau=[0.256, 0.512, 0.768] ,amp=[1.0, 0.5, -1.0], p2=[0.45,0.0, -0.45], 
-#p1=[0.015,0.0, -0.015], dt=dt, nt=512,  
-#dx1=10.0, nx1=128, dx2=10.0, nx2=128, f0= 25.0 );
+d=SeisParabEvents(tau=[0.256, 0.512, 0.768] ,amp=[1.0, 0.5, -1.0], p2=[0.45,0.0, -0.45], 
+p1=[0.015,0.0, -0.015], dt=dt, nt=512,  
+dx1=10.0, nx1=128, dx2=10.0, nx2=128, f0= 25.0 );
 
 
-d = SeisLinearEvents(;nt=250, nx1=128,dx1=12.5, nx2=128,dx2=12.5,tau=[0.25,0.4],p1=[-0.0001,-0.00015],p2=[0.0001,0.00015]);
+#d = SeisLinearEvents(;nt=250, nx1=128,dx1=12.5, nx2=128,dx2=12.5,tau=[0.25,0.4],p1=[-0.0001,-0.00015],p2=[0.0001,0.00015]);
 dobs = copy(d);
 
 #shot=d;
@@ -49,7 +49,7 @@ Dict(:patch_size=>patch_size, :Noverlap=>Noverlap, :dims=>dims, :normalize=>true
 println("4) Reconstruction of the data: Inversion of the coefficients")
 
 m1, J = ISTA(x0,dobs,operators,parameters,μ=0.5,Ni=25,tolerance=0.00001)
-d_rec1= real(LocalFFTOp(m1,false; patch_size, Noverlap, dims, normalize=true, padd=true));
+d_rec1= real(LocalFFTOp(m1,false; patch_size, Noverlap, dims, normalize=true, padd=false));
 
 
 #m2, J = FISTA(x0,dobs,operators,parameters,0.5,50,0.0001);
