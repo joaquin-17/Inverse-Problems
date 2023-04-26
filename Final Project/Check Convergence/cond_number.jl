ce50ca4efadcd831af069214c2f4bd5d861b73ea
Joaquin Acedo <aacedo@ualberta.ca>
2023-04-25 18:59:50 -0600

    images

--- Final Project/Check Convergence/cond_number.jl
+++ Final Project/Check Convergence/cond_number.jl
@@ -3,7 +3,8 @@ function k(G::Matrix, ρ::Float64)
     I=diagm(ones(size(G,1)));
     E=eigen(G+ ρ*I);
     λ=E.values;
-    κ= (maximum(λ)+ ρ )/ (minimum(λ) + ρ)
+    κ=cond(G+ ρ*I)   
+    #κ= (maximum(λ)+ ρ )/ (minimum(λ) + ρ)
 
     return λ, κ
 
--- Final Project/Check Convergence/kronecker_reconstruction.jl
+++ Final Project/Check Convergence/kronecker_reconstruction.jl
@@ -2,8 +2,8 @@ using PyPlot, FFTW, DSP, SeisProcessing, SeisPlot, LinearAlgebra, HDF5 #SeisReco
 
 
 
-#include("/home/aacedo/Desktop/GitHub/Inverse-Problems/Final Project/Tools.jl")
-include("C:\\Users\\Joaquin\\Desktop\\Research\\Codes\\Inverse-Problems\\Final Project\\Tools.jl")
+include("/home/aacedo/Desktop/GitHub/Inverse-Problems/Final Project/Tools.jl")
+#include("C:\\Users\\Joaquin\\Desktop\\Research\\Codes\\Inverse-Problems\\Final Project\\Tools.jl")
 
 
 include("CG.jl");
@@ -20,9 +20,10 @@ shot=shot*(10^2)
 nt,nr=size(shot);
 d_obs = copy(shot);
 
+#=
 for i=1:nr
        p = rand()
-            if   p < 0.3
+            if   p < 0.5
                 d_obs[:,i] .= 0.0
             end
 end
@@ -30,11 +31,11 @@ end
   
 S = CalculateSampling(d_obs);
 d_obs = S.*shot; 
+=#
 
-
-#for j=1:4:size(d_obs,2)
- #   d_obs[:,j] .= 0.0
-#end
+for j=1:4:size(d_obs,2)
+    d_obs[:,j] .= 0.0
+end
 
 
 r=SamplingVector(d_obs[64,:])
@@ -51,9 +52,8 @@ d=reshape(d_obs,length(d_obs)); # shot as a vector
 A=kronRT*kronFxFt;
 y=kronRT*d;
 ρ=1.0
-G=A'*A; 
+G=A*A'; 
 
-#=
 ρ=[0.0, 0.001, 0.01, 0.1, 1,10, 100]
 λ1,cn1=k(G,ρ[1]);
 λ2,cn2=k(G,ρ[2]);
@@ -65,7 +65,7 @@ G=A'*A;
 #λp5,cn=k(G,0.0);
 cn=[cn1,cn2,cn3,cn4,cn5,cn6,cn7]
 
-figure(1);
+figure(1, figsize=(10,5));
 
 plot(sort(λ1,rev=true),label="ρ=0.0");
 plot(sort(λ2,rev=true),label="ρ=0.001");
@@ -78,19 +78,28 @@ plot(sort(λ7,rev=true),label="ρ=100.0");
 grid("True");
 ylabel("λ",fontsize=15);
 xlabel("n",fontsize=15);
-ylim([-10, 150])
+#ylim([-10, 150])
 legend()
 
 
 
-figure(2);
+figure(2,figsize=(8,5));
 
-plot(ρ,round.(cn,digits=5))
+plot( ρ[1],round(cn1,digits=3),"o",label="ρ=0.0");
+plot(ρ[2],round(cn2,digits=3),"o",label="ρ=0.001");
+plot(ρ[3],round(cn3,digits=3),"o",label="ρ=0.01");
+plot(ρ[4],round(cn4,digits=3),"o",label="ρ=0.1");;
+plot(ρ[5],round(cn5,digits=3),"o",label="ρ=1.0");;
+plot(ρ[6],round(cn6,digits=3),"o",label="ρ=10.0");
+plot(ρ[7],round(cn7,digits=3),"o",label="ρ=100.0");
+#plot(ρ,round.(cn,digits=5),"o")
 xlabel("ρ",fontsize=15)
 ylabel("κ(ρ)",fontsize=15)
 grid("True")
-=#
+legend()
 
+
+#=
 xls= (G+ ρ*diagm(ones(size(G,1))))\ A'*y;
 m0=zeros(length(d_obs));
 z=copy(m0);
@@ -100,7 +109,7 @@ yc=vcat(y, sqrt(ρ)* (z))
 
 xcg, Jcg= CG(Ac,yc; x0=m0, Ni=50, tol=1.0e-15)
 m, J=  ADMM(A,y; x0= 0.0, ρ= 2.0, λ=4.0, Ni=150, Ne=50, tol=1.0e-8)
-#=
+
 m=reshape(m, size(shot))
 =#
 #=
